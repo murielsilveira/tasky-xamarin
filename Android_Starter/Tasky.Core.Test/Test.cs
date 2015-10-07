23a24a431330bec2dd08679b3a23492dd559f83e
Muriel Silveira <murielkong@gmail.com>
2015-10-07 13:13:42 -0300

    Create a test project

---
+++ Android_Starter/Tasky.Core.Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tasky.Core.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+    }
+}
+
